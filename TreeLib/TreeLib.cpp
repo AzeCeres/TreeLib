f665353225ee8ffcb4e5cd61b1ea3ac0896ac2c4
AzeCeres <juliantokle@gmail.com>
2023-10-30 00:06:15 +0100

    Tree

--- TreeLib/TreeLib.cpp
+++ TreeLib/TreeLib.cpp
@@ -3,6 +3,7 @@
 
 #include "TreeLib.h"
 #include <iostream>
+#include <string>
 using namespace std;
 /**
  * Test Function to see if the library is working properly
@@ -14,29 +15,77 @@ int main() {
      *                 1
      *             /   |   \
      *            2    3    4
+     *                     / \
+     *                    5   6
+     *                         \
+     *                          7
+     */
+    /* How the Tree *should* look after delete
+     *                1
+     *             /    \
+     *            2      3
      */
     Node* tempNode = CreateNewNode(tree, 1); // This will become the root.
-    cout << isRoot(tempNode); //Should be true since it has no Parent
-    cout << isLeaf(tempNode); //Should be true since it has no Children yet
+    cout << tempNode->isRoot(); //Should be true since it has no Parent
+    cout << tempNode->isLeaf(); //Should be true since it has no Children yet
     CreateNewNode(tempNode, 2);
     CreateNewNode(tempNode, 3);
-    CreateNewNode(tempNode, 4); //All direct Children of the root
-    cout << isRoot(tempNode); //Should still be true since it still has no Parent
-    cout << isLeaf(tempNode); //Should be false since it has Children now
-    cout << (getRoot(tree)== tempNode); // Should be true, the root should not have moved out of the first position
-    //Expected output 11101
+    Node* child4 = CreateNewNode(tempNode, 4); //All direct Children of the root
+    cout << tempNode->isRoot(); //Should still be true since it still has no Parent
+    cout << tempNode->isLeaf(); //Should be false since it has Children now
+    cout << (tree.GetRoot()== tempNode); // Should be true, the root should not have moved out of the first position
+    cout << (tempNode->GetRoot() == tempNode); // Should be true, the root should not have moved out of the first position
+    cout << endl << "Depth:" << endl;
+    cout << Depth(child4); //Should be 1
+    cout << Depth(tempNode); //Should be 0
+    CreateNewNode(child4, 5);
+    Node* child6 = CreateNewNode(child4, 6);
+    cout << Depth(child6); //Should be 2
+    Node* child7 = CreateNewNode(child6, 7);
+    cout << Depth(child7); //Should be 3
+    cout << endl << "Parent Num:" << endl;
+    cout << "7's parent: "<<child7->GetParent()->GetNum(); //Should be 6
+    cout << "'s parent: " << child6->GetParent()->GetNum(); //Should be 4
+    cout << "'s parent: " << child4->GetParent()->GetNum(); //Should be 1
+    cout << " has parent? = "<<tempNode->HasParent() << endl; //Should be false / 0
+    cout << endl << "Nodes in tree before deletion" << endl;
+    vector<Node*> nodes = tree.GetNodes();
+    for(Node* node : nodes){
+     cout << node->GetNum()<< " ";// Should be 1 2 3 4 5 6 7
+    }
+ cout << endl << "Children of root before deletion" << endl;
+    nodes = tree.GetRoot()->GetChildren();
+    for(Node* node : nodes){
+     cout << node->GetNum()<< " ";// Should be 2 3 4
+    }
+    child4->DeleteNode();
+     cout << endl << "Nodes in tree after deletion" << endl;
+    nodes = tree.GetNodes();
+    for(Node* node : nodes){
+     cout << node->GetNum()<< " ";// Should be 1 2 3
+    }
+ cout << endl << "Children of root after deletion" << endl;
+ nodes = tree.GetRoot()->GetChildren();
+    for(Node* node : nodes){
+     cout << node->GetNum()<< " ";// Should be 2 3
+    }
+    // Expected output:
+    // 111011
+    // Depth:
+    // 1023
+    // Parent Num:
+    // 7's parent: 6's parent: 4's parent: 1 has parent? = 0
+    // 
+    // Nodes in tree before deletion
+    // 1 2 3 4 5 6 7
+    // Children of root before deletion
+    // 2 3 4
+    // Nodes after deletion
+    // 1 2 3
+    // Children of root after deletion
+    // 2 3
+    
     return 0;
-}
-
-bool isRoot(Node* node) {
-    if (node == node->tree->nodes.at(0))
-        return true;
-    return false;
-}
-bool isLeaf(Node* node) {
-    if (node->children.empty())
-        return true;
-    return false;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- TreeLib/TreeLib.h
+++ TreeLib/TreeLib.h
@@ -1,63 +1,182 @@
 ﻿#pragma once
 #include <climits>
+#include <queue>
+#include <string>
 #include <vector>
 using namespace std;
 
 void hello();
 
 class Tree;
-
+Tree newTree();
 class Node {
 public:
     int num{INT_MIN};
     vector<Node*> children;
-    Tree* tree;
+    Node* parent{nullptr};
+    Tree* tree{nullptr};
+    bool isLeaf() const {
+        return children.empty();
+    }
+    bool isRoot() const; // Declared here
+    Node* GetRoot(); // Declared here
+    vector<Node*> GetChildren() const {
+        return children;
+    }
+    int GetNum() const {
+        return num;
+    }
+    void SetNum(int value) {
+        num = value;
+    }
+    bool HasParent() const {
+        return parent != nullptr;
+    }
+    Node* GetParent() const {
+        if (HasParent())
+            return parent;
+        return nullptr;
+    }
+    void SetParent(Node* node) {
+        parent = node;
+    }
+    Tree* GetTree() const {
+        return tree;
+    }
+    void SetTree(Tree* tree) {
+        this->tree = tree;
+    }
+    int GetDepth();
+    void DeleteNode();
+    void RemoveChild(Node* const childToRemove){
+        for (int i = 0; i < children.size(); ++i) {
+            if (children.at(i) != childToRemove) continue;
+            children.erase(children.begin() + i);
+            break;
+        }
+    }
 };
 class Tree {
 public:
     vector<Node*> nodes;
+    Node* GetRoot(); // Declared here
+    vector<Node*> GetNodes() const {
+        return nodes;
+    }
+    int GetSize() const {
+        return nodes.size();
+    }
+    bool isEmpty() const {
+        return nodes.empty();
+    }
+    void DeleteTree() {
+        for (Node* node : nodes) {
+            node->DeleteNode();
+        }
+    }
+    void RemoveNode(Node* nodeToRemove)
+    {
+        for (int i = 0; i < nodes.size(); ++i) {
+            if (nodes.at(i) == nodeToRemove) {
+                nodes.erase(nodes.begin() + i);
+                break;
+            }
+        }
+    }
 };
-Node* getRoot(Tree tree)
-{
-    if(tree.nodes.empty()) return nullptr;
-    tree.nodes.at(0);
+Node* Tree::GetRoot() { //Defined for Tree
+
+    return nodes.at(0);
 }
-Tree newTree()
-{
+Node* Node::GetRoot(){ //Defined for Node
+    return tree->nodes.at(0);
+}
+inline bool Node::isRoot() const{ //Defined for Node
+    return tree->nodes.at(0) == this;
+}
+
+Tree newTree(){
     Tree* temp = new Tree;
     temp->nodes.clear();
     return *temp;
 }
-bool isRoot(Node* node);
-bool isLeaf(Node* node);
-Node* newNode(int value) {
+
+Node* newNode(int value) { //todo move into Node class
     Node* temp = new Node;
     temp->num = value;
     return temp;
 }
-Node* CreateNewNode(Node* &parentNode, int value) {
+Node* CreateNewNode(Node* &parentNode, int value) { 
     Node* tempNode = newNode(value);
     tempNode->num = value;
+    tempNode->SetParent(parentNode);
     Tree* tempTree = parentNode->tree;
     tempNode->tree = tempTree;
     (tempTree->nodes).push_back(tempNode);
     (parentNode->children).push_back(tempNode);
     return tempNode;
 }
-Node* CreateNewNode(Tree &tree, int value) {
-    if (!tree.nodes.empty()) {
-        Node* tempNode{nullptr};
+Node* CreateNewNode(Tree &tree, int value) { 
+    if (tree.nodes.empty()) {
+        // If the tree is empty, create a root
+        Node* tempNode = newNode(value);
+        tempNode->tree = &tree; // Set the tree pointer for the root node
+        tree.nodes.push_back(tempNode);
+        return tempNode;
+    } else {
+        Node* tempNode = nullptr;
         for (int i = 0; i < tree.nodes.size(); ++i) {
-            if (tree.nodes.at(i) != nullptr) //todo Unfinished logic
-                continue;
-            tempNode = CreateNewNode(tree.nodes.at(i), value);
+            if (tree.nodes.at(i) == nullptr) {
+                // Initialize the tree.nodes vector with nullptrs
+                tempNode = CreateNewNode(tree.nodes.at(i), value);
+                break;
+            }
+        }
+
+        if (tempNode == nullptr) {
+            // If no nullptr was found, create a new node
+            tempNode = newNode(value);
+            tempNode->tree = &tree;
+            tree.nodes.push_back(tempNode);
         }
         return tempNode;
     }
-    // If the tree is empty it has to create a root
-    Node* tempNode = newNode(value);
-    tempNode->num = value;
-    (tree.nodes).push_back(tempNode);
-    return tempNode;
 }
-int Depth(Node node);
+void Node::DeleteNode() {
+    if (this == nullptr) {
+        return;
+    }
+
+    // Remove the node from its parent's children list
+    if (parent) {
+        parent->RemoveChild(this);
+    }
+    if(tree){
+        // Remove the node from the tree's nodes list
+        tree->RemoveNode(this);
+    }
+    // Recursively delete the node's children
+    for (Node* child : children) {
+        child->DeleteNode();
+    }
+
+    // Delete the node itself
+    delete this;
+}
+int Depth(Node* node) {
+    if (node->isRoot())
+        return 0; // Root nodes have depth 0.
+
+    int depth = 0;
+    Node* currentNode = node;
+    
+    while (currentNode->GetParent() != nullptr) {
+        depth++;
+        currentNode = currentNode->GetParent();
+    }
+    return depth;
+}
+
+int Node::GetDepth()  {
+    return Depth(this);
+}
